8536cb08de484a76be72c5c3ac476e89dbd11415
Sabrina Ip <kkip90@gmail.com>
2016-11-09 08:29:36 -0500

    refactored minMax function

--- BuyersNomorse/BuyersNomorse/ResultsViewController.swift
+++ BuyersNomorse/BuyersNomorse/ResultsViewController.swift
@@ -20,7 +20,15 @@ class ResultsViewController: UIViewController, UITextFieldDelegate, UITableViewD
     var searchedItem = ""
     
     var endpoint: String {
-        return constructEndpoint(minPrice: self.minPrice, maxPrice: self.maxPrice)
+        let keywordInput = self.searchedItem.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+        var webAddress = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=SabrinaI-GroupPro-PRD-dbff3fe44-d9ad0129&RESPONSE-DATA-FORMAT=JSON&paginationInput.entriesPerPage=25&keywords=\(keywordInput)"
+        if let maxPriceEntered = self.maxPrice {
+            webAddress += "&itemFilter(0).name=MaxPrice&itemFilter(0).value=\(maxPriceEntered)"
+        }
+        if let minPriceEntered = self.minPrice {
+            webAddress += "&itemFilter(1).name=MinPrice&itemFilter(1).value=\(minPriceEntered)"
+        }
+        return webAddress
     }
     
     var itemSelected: SearchResults?
@@ -42,21 +50,17 @@ class ResultsViewController: UIViewController, UITextFieldDelegate, UITableViewD
         }
     }
     
-    func constructEndpoint(minPrice: String?, maxPrice: String?) -> String {
-        let keywordInput = self.searchedItem.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        
-        var endpoint = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=SabrinaI-GroupPro-PRD-dbff3fe44-d9ad0129&RESPONSE-DATA-FORMAT=JSON&paginationInput.entriesPerPage=25&keywords=\(keywordInput)"
-        
-        if let maxPriceEntered = maxPrice {
-            endpoint += "&itemFilter(0).name=MaxPrice&itemFilter(0).value=\(maxPriceEntered)"
-        }
-        
-        if let minPriceEntered = minPrice {
-            endpoint += "&itemFilter(1).name=MinPrice&itemFilter(1).value=\(minPriceEntered)"
-        }
-        
-        return endpoint
-    }
+//    func constructEndpoint() -> String {
+//        let keywordInput = self.searchedItem.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+//        var webAddress = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=SabrinaI-GroupPro-PRD-dbff3fe44-d9ad0129&RESPONSE-DATA-FORMAT=JSON&paginationInput.entriesPerPage=25&keywords=\(keywordInput)"
+//        if let maxPriceEntered = self.maxPrice {
+//            webAddress += "&itemFilter(0).name=MaxPrice&itemFilter(0).value=\(maxPriceEntered)"
+//        }
+//        if let minPriceEntered = self.minPrice {
+//            webAddress += "&itemFilter(1).name=MinPrice&itemFilter(1).value=\(minPriceEntered)"
+//        }
+//        return webAddress
+//    }
     
     
     @IBAction func minPriceChanged(_ sender: UITextField) {
@@ -64,55 +68,86 @@ class ResultsViewController: UIViewController, UITextFieldDelegate, UITableViewD
     @IBAction func maxPriceChanged(_ sender: UITextField) {
     }
     
-    func isMinMaxFieldCheckPassed() -> Bool {
-        var check = false
-        //couldn't find a way to cast as a double
-        if let minString = minPriceTextField.text, let maxString = maxPriceTextField.text {
-            if Int(minString)! < Int(maxString)! {
-                check = true
-            }
-            else {
-                check = false
-            }
-        }
-        return check
-    }
+    /*
+     Int(minString)! and Int(maxString)! can cause an error
+     when user enters something other than a number, or an empty string.
+     Need to guard against those cases first
+        -Also, should we allow for it to search if only one number is entered? (ex. max  is 10, min is not entered)
+     */
     
+//    func isMinMaxFieldCheckPassed() -> Bool {
+//        var check = false
+//        //couldn't find a way to cast as a double
+//        if let minString = minPriceTextField.text, let maxString = maxPriceTextField.text {
+//            if Int(minString)! < Int(maxString)! {
+//                check = true
+//            }
+//            else {
+//                check = false
+//            }
+//        }
+//        return check
+//    }
     
-    @IBAction func doneButtonTapped(_ sender: UIButton) {
+    func minMaxAreAcceptableAnswers() -> Bool {
+        var minPDouble: Double?
+        var maxPDouble: Double?
         
-        if isMinMaxFieldCheckPassed() {
-            
-            errorLabel.isHidden = true
-            
-            if minPriceTextField.text! != "" {
-                guard let minNum = Double(minPriceTextField.text!)
-                    else {
-                        print("minPrice field is not a num: \(minPriceTextField.text!)")
-                        return
-                }
-                minPrice = String(minNum)
+        if minPriceTextField.text! != "" {
+            guard let minNum = Double(minPriceTextField.text!) else {
+                print("minPrice field is not a num: \(minPriceTextField.text!)")
+                errorLabel.text = "The minimum price is not a valid answer"
+                return false
             }
-            
-            if maxPriceTextField.text! != "" {
-                guard let maxNum = Double(maxPriceTextField.text!)
-                    else {
-                        print("maxPrice field is not a num: \(maxPriceTextField.text!)")
-                        return
-                }
-                maxPrice = String(maxNum)
-            }
-            
-            print("min price is \(minPrice)")
-            print("max price is \(maxPrice)")
+            minPDouble = minNum
+            minPrice = String(minNum)
         }
-            
-        else {
+        
+        if maxPriceTextField.text! != "" {
+            guard let maxNum = Double(maxPriceTextField.text!) else {
+                print("maxPrice field is not a num: \(maxPriceTextField.text!)")
+                errorLabel.text = "The maximum price is not a valid answer"
+                return false
+            }
+            maxPDouble = maxNum
+            maxPrice = String(maxNum)
+        }
+
+        if let minExists = minPDouble, let maxExists = maxPDouble {
+            guard minExists < maxExists else {
+                errorLabel.text = "Minimum should be less than Maximum"
+                return false
+            }
+        }
+        
+        print("min price is \(minPrice)")
+        print("max price is \(maxPrice)")
+        return true
+    }
+
+
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        print("Min price textfield is showing \(dump(minPriceTextField))")
+        print("Max price textfield is showing \(dump(maxPriceTextField))")
+        
+        guard minMaxAreAcceptableAnswers() else {
             errorLabel.isHidden = false
-            errorLabel.text = "Minimum should be less than Maximum"
+            return
         }
-        // Need to figure out how to guard min price < max price
-        // Need to update tableview to new prices
+        errorLabel.isHidden = true
+        print("The endpoint is currently \(self.endpoint)")
+        
+        /* The error appears in that min price can't be entered without a max price
+            -Can we do any JSON manipulation so that we can do this?
+            -Otherwise, we'll need to change code, where we only accept if both min and max are enetered
+            -JSON parsing error for one example I tried - update SearchResults.swift to be more robust-
+                    http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=SabrinaI-GroupPro-PRD-dbff3fe44-d9ad0129&RESPONSE-DATA-FORMAT=JSON&paginationInput.entriesPerPage=25&keywords=wor&itemFilter(0).name=MaxPrice&itemFilter(0).value=30.0&itemFilter(1).name=MinPrice&itemFilter(1).value=20.0
+        */
+        loadData()
+        
+//     Thinking of resetting to nil... seems like when I set a max and min, and then take away a value, the max value still stays
+//        minPrice = nil
+//        maxPrice = nil
     }
     
     // MARK: - TABLEVIEW
