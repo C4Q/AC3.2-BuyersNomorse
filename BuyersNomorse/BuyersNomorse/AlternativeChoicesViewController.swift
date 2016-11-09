ca10765a21ad5a0973b4d58b51b04d9e51d3d6dc
Madushani Lekam Wasam Liyanage <Shashi@Shashis-MacBook.local>
2016-11-09 12:42:07 -0500

    Changes enum->tuple

--- BuyersNomorse/BuyersNomorse/AlternativeChoicesViewController.swift
+++ BuyersNomorse/BuyersNomorse/AlternativeChoicesViewController.swift
@@ -6,6 +6,52 @@
 //  Copyright © 2016 Sabrina, Shashi. All rights reserved.
 //
 
+fileprivate Catagory: String {
+    
+    (antiques, "20081"
+    case art = "550"
+    case baby = "2984"
+    case books = "267"
+    case businessAndIndustrial = "12576"
+    case camerasAndPhoto = "625"
+    case cellphonesAndAccessories = "15032"
+    case clothingShoesAndAccessories = "11450"
+    case coinsAndPaperMoney = "11116"
+    case collectibles = "1"
+    case computersTabletsAndNetworking = "58058"
+    case consumerElectronics = "293"
+    case crafts = "14339"
+    case dollsAndBears = "237"
+    case moviesAndDVDs = "11232"
+    case entertainmentMemorabilia = "45100"
+    case everythingElse = "99"
+    case giftCardsAndCoupons = "172008"
+    case healthAndBeauty = "26395"
+    case homeAndGarden = "11700"
+    case jewelryAndWatches = "281"
+    case music = "11233"
+    case musicalInstrumentsAndGear = "619"
+    case petSupplies = "1281"
+    case potteryAndGlass = "870"
+    case realEstate = "10542"
+    case specialtyServices = "316"
+    case sportingGoods = "888"
+    case sportsMemCardsAndFanShop = "64482"
+    case stamps = "260"
+    case ticketsAndExperiences = "1305"
+    case toysAndHobbies = "220"
+    case travel = "3252"
+    case videoGamesAndConsoles = "1249"
+
+}
+
+fileprivate func randomCatogoryGenerator() -> String {
+   let rand = Int(arc4random_uniform(35))
+    var catogoryString: String!
+    
+  }
+
+
 import UIKit
 
 class AlternativeChoicesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
@@ -20,6 +66,8 @@ class AlternativeChoicesViewController: UIViewController, UICollectionViewDelega
         
         /*We need to figure out another way to do randomNum - make sure the categoryId's exist*/
         /* Got error message from ebay: Submitted category [960] has expired. Try again with new category [74969] */
+        
+        
         
         /* maybe make enums for these category nums: http://www.isoldwhat.com/getcats/ */
         
